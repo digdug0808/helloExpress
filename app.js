f9dc5da2875f14cacecd2966145ff18061eed3ee
catetcpasswd <randy.gonzales@gmail.com>
2013-08-15 20:35:44 +0800

    simply version of app.js. learning routes

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-
+/* simplified app.js
+   for the routes demo
+ */
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+  , http = require('http');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000)
+});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get("/", function(req,res) {
+    res.send("Hello Express from route!");
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
